33384c62efa6d82faccb4d8cfbf1249bbe0265c8
haruto89610 <78660144+haruto89610@users.noreply.github.com>
2022-04-03 14:30:02 +0900

    transpose rotational matrix

--- src/JetsonNano/cpp/main.cpp
+++ src/JetsonNano/cpp/main.cpp
@@ -2,25 +2,28 @@
 // g++ -o pug --std=c++11 main.cpp -lm
 
 #include <iostream>
-#include "rt.hpp"
+#include "rtmatrix.h"
 
 using namespace linalg::ostream_overloads;
 
 int main(int argc, const char * argv[]) {
 
-  Position    position;
-  Orientation orientation;
-
-  {
-    RTMatrix m{ orientation, position };
-    std::wcout << m.rotate() << std::endl;
+  Orientation o;
+  Position    p;
+  Coordinate  c;
+  RTMatrix rt(o, p, c);
+  while (true) {
+    o.roll  = 1.0;
+    o.pitch = 1.0;
+    o.yaw   = 1.0;
+    p.x = 0.0;
+    p.y = 0.0;
+    p.z = 0.0;
+    c.x = 1.0;
+    c.y = 1.0;
+    c.z = 1.0;
+    rt.update(o, p, c);
+    std::wcout << rt.transform() << std::endl;
   }
-
-  {
-    orientation.roll = 1.0;
-    RTMatrix m{ orientation, position };
-    std::wcout << m.X() << std::endl;
-  }
-
   return 0;
 }
