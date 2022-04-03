fd6f59f6ca6d9d5da7839a69567d327e9bc2e384
haruto89610 <78660144+haruto89610@users.noreply.github.com>
2022-04-03 21:07:25 +0900

    Debug IK.h

--- src/JetsonNano/cpp/IK.h
+++ src/JetsonNano/cpp/IK.h
@@ -2,23 +2,21 @@
 
 #pragma once
 
-#include "rtmatrix.hpp"
-
-struct RT {
+struct IK {
 
   static auto solve_R(const Position& coord, float coxa, float femur, float tibia) -> float3 {
     auto domain { get_domain(coord, coxa, femur, tibia) };
     auto gamma  { get_gamma(domain) };
-    auto theta  { get_theta(coord.y, coord.z, domain, -coxa) };
-    auto alpha  { get_alpha(coord.x, coord.y, coord.z, domain, gamma, femur, tibia) };
+    auto theta  { get_theta(coord, domain, -coxa) };
+    auto alpha  { get_alpha(coord, domain, gamma, coxa, femur, tibia) };
     return float3{ -theta, alpha, gamma };
   }
 
   static auto solve_L(const Position& coord, float coxa, float femur, float tibia) -> float3 {
     auto domain { get_domain(coord, coxa, femur, tibia) };
     auto gamma  { get_gamma(domain) };
-    auto theta  { get_theta(coord.y, coord.z, domain, coxa) };
-    auto alpha  { get_alpha(coord.x, coord.y, coord.z, domain, gamma, femur, tibia) };
+    auto theta  { get_theta(coord, domain, coxa) };
+    auto alpha  { get_alpha(coord, domain, gamma, coxa, femur, tibia) };
     return float3{ -theta, alpha, gamma };
   }
 
@@ -33,12 +31,12 @@ private:
     return std::atan2(-std::sqrt(1 - d*d), d);
   }
 
-  static auto get_theta(float y, float z, float d, float coxa) -> float {
-    return -std::atan2(z, y) - std::atan2(std::sqrt(y*y + z*z - coxa*coxa), coxa);
+  static auto get_theta(const Position& coord, float d, float coxa) -> float {
+    return -std::atan2(coord.z, coord.y) - std::atan2(std::sqrt(coord.y*coord.y + coord.z*coord.z - coxa*coxa), coxa);
   }
 
-  static auto get_alpha(float x, float y, float z, float d, float gamma, float femur, float tibia) -> float {
-    return std::atan2(-x, std::sqrt(y*y + z*z)) - std::atan2(tibia * sin(gamma), femur + tibia + std::cos(gamma));
+  static auto get_alpha(const Position& coord, float d, float gamma, float coxa, float femur, float tibia) -> float {
+    return std::atan2(-coord.x, std::sqrt(coord.y*coord.y + coord.z*coord.z - coxa*coxa)) - std::atan2(tibia * sin(gamma), femur + tibia * std::cos(gamma));
   }
 
   static auto checkdomain(const float& domain) -> float {
